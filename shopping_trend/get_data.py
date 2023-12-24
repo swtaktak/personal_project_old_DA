0a8965c9c67a3ad5fe220b43df9af1cbe550f9d5
SeongwonTak <taraki3639@gmail.com>
2023-12-24 21:24:40 +0900

    2023_1224
    - datacode 간단한 테스트
    - 오라클 연동 간단 실습(쿼리 검색까지)

--- shopping_trend/get_data.py
+++ shopping_trend/get_data.py
@@ -1,10 +1,7 @@
 # data get
 # 목표 : 네이버 api를 활용한 검색어 데이터 get 및 전처리
 import requests
-import os
-import sys
 import json
-import urllib.request
 import pandas as pd
 
 client_df = pd.read_csv('C:/Users/USER/Documents/client_secret.csv')
@@ -39,9 +36,14 @@ endDate='2023-09-30'
 timeUnit='month'  #'date','week','month'
 category=[
     {'name':'패션의류', 'param':['50000000']},
+    # 세부 품목별 카테고리 가능, 다만 이것에 대해서 정확한 이름을 붙여야함
+    # 직접 검색을 통해 확인해야 한다고 가이드에 적혀 있어 대상을 한정하여 검색 필요
+    {'name': '식품', 'param':['50007077']}
 ]
 device='pc'  #'pc','mo'
-gender='f'   #'m','f'
+gender='m,f'   #'m','f'
 ages=['20']  #10, 20, 30, 40, 50, 60(60은 60세 이상)
 
 result = get_result(startDate,endDate,timeUnit,category,device,gender,ages)
+
+# json을 dataframe 형태로 저장하여 바꾸기 -> json_normalize
