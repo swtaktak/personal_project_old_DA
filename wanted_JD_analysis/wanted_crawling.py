c4ed1c6bf697397f163a3c73663fb73e99b96e81
SeongwonTak <taraki3639@gmail.com>
2024-01-05 20:51:25 +0900

    240105
    전처리_시각화진행_tf_idf upload

--- wanted_JD_analysis/wanted_crawling.py
+++ wanted_JD_analysis/wanted_crawling.py
@@ -9,8 +9,6 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
 import json
-# bs4로 되는지 체크를 위한 간단한 정적크롤링 작성
-# 이후에는 링크 전체를 돌려서 할 거임
 job_df = pd.DataFrame(columns = ['company_name', 'descriptions','date',
                                  'industry', 'category', 'title'])
 cur_row = 0
@@ -44,7 +42,5 @@ while cur_row < 2500:
 
 job_df.to_csv('C:/Users/USER/Desktop/personal_project/wanted_crawling.csv',
               encoding = 'utf-8-sig', index = False)
-# 문제는 공고 번호가 너무 띄엄띄엄 있어서 이렇게 돌리면 비효율적이긴 함
-# 다만 전체 모든 영역에 대해 동적 크롤링을 돌리기도 비효율적, 최신 범위를 잡기 어려움
-# 비효율적이이도 이게 최선.
+
 
